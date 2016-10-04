ad3f4811956a1c547b95da8c187c24a722aecd94
st921 <st921@DESKTOP-2FQ1DIR>
2016-10-05 00:02:30 +0800

    hw1

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -1,10 +1,14 @@
 package fcu.iecs.oop;
-
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello World");
+          String lyrics = "Let it go!Let it go!Connot hold it back anymore";
+          System.out.println(lyrics);
+          String lyrics1 = lyrics.replace("Connot","can't");
+
+          System.out.println(
+        		  lyrics1.replace("it","\"her\""));
 	}
 
 }
